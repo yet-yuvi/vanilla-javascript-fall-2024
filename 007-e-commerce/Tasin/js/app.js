ad1db528066d3be5e95d6d8269d085dadb443b7b
TasinLunatic <ahmedtasin207@gmail.com>
2025-11-25 20:48:14 +0800

    modified index section

---
+++ 007-e-commerce/Tasin/js/app.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    id: 1,
+    name: "Thinkpad Laptop",
+    price: 1500,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Laptops", ],
+  },
+  {
+    id: 2,
+    name: "Wireless Mouse",
+    price: 50,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Accessories", "Peripherals"],
+  },
+  {
+    id: 3,
+    name: "Mechanical Keyboard",
+    price: 100,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Accessories", "Peripherals"],
+  },
+  {
+    id: 4,
+    name: "External Hard Drive",
+    price: 120,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Storage", "Accessories"],
+  },
+  {
+    id: 5,
+    name: "Graphics Card",
+    price: 500,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Components", "Gaming"],
+  },
+  {
+    id: 6,
+    name: "Portable SSD",
+    price: 200,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Storage", "Accessories"],
+  },
+  {
+    id: 7,
+    name: "Gaming Monitor",
+    price: 300,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Monitors", "Gaming"],
+  },
+  {
+    id: 8,
+    name: "All-in-One Printer",
+    price: 150,
+    image: "./assets/images/Best-laptops-Indybest-review.webp",
+    categories: ["Peripherals", "Printers"],
+  },
+];
+
+const renderProducts = (products) => {
+   
+}
+
+render Products(products);
